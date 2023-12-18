5840707af8a62c843bace70f7dcffa34bb306902
Nikita_Skodin <skodin36@gmail.com>
2023-12-18 17:44:10 +0300

    commit

---
+++ src/main/java/com/skodin/TimeTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.skodin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeTrackerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skodin/TimeTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skodin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
